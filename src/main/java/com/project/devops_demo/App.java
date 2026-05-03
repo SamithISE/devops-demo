785c2cf39a695c112f584ba3ab84c41c4cbff609
SamithISE <vvce23ise0219@vvce.ac.in>
2026-05-03 22:00:31 +0530

    Initial commit

---
+++ src/main/java/com/project/devops_demo/App.java
@@ -0,0 +1,10 @@
+package com.project.devops_demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Buddy + Maven CI/CD Project Running Successfully");
+    }
+}
